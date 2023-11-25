9c66a51a28e0c2638c459e24955c3822f0f82e9b
Mladen Milankovic <developer@mmlado.com>
2023-11-25 17:51:31 +0100

    Finish game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let number = rand::thread_rng().gen_range(1..100);
+    loop {
+        let input = get_input();
+
+        if input.trim() == "exit" {
+            println!("Exiting...");
+            break;
+        }
+        
+        let guess: i32;
+        match input.trim().parse::<i32>() {
+            Ok(n) => guess = n,
+            Err(e) => {
+                println!("Must be number or exit: {}", e);
+                continue;
+            },
+        }
+
+        if check(number, guess) {
+            break;
+        };
+    }
+}
+
+fn get_input() -> String {
+    println!("Guess the number (exit): ");
+    let mut input = String::new();
+    let _ = io::stdin().read_line(&mut input);
+    input
+}
+
+fn check(number: i32, guess: i32) -> bool {
+    if guess == number {
+        println!("You guessed right");
+        return true;
+    }
+    
+    if guess > number {
+        println!("Guess lower!")
+    }
+    else {
+        println!("Guess higher!")
+    }
+
+    false
 }
